73e4edce2f2948263588e630da829f88c1f4cd04
Jming1999 <jming_xu@163.com>
2017-12-16 12:29:47 +0800

    加入了新的代码

---
+++ 微信/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微信
+//
+//  Created by 许佳铭 on 2017/12/13.
+//  Copyright © 2017年 许佳铭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微信/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  微信
+//
+//  Created by 许佳铭 on 2017/12/13.
+//  Copyright © 2017年 许佳铭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
